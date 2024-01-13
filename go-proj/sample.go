2a8b34935f928220ba5ad22b12ef61f841a009e1
Nguyen Le <lenguyen_d@hotmail.com>
2024-01-13 14:48:37 -0800

    Update 1st go program with comment

--- go-proj/sample.go
+++ go-proj/sample.go
@@ -6,7 +6,14 @@ package main
 
 import "fmt"
 
+// simple_func is a function that prints the first 10 numbers in the Fibonacci sequence.
+// It starts with 1 and 1, and each subsequent number is the sum of the previous two numbers.
 func simple_func() {
+	// document what a, b, c are in fibonacci sequence calculation
+
+	// initialize a, b, c
+	// use a for loop to calculate the first 10 numbers in the Fibonacci sequence
+	// print the first 10 numbers in the Fibonacci sequence
 	var a, b, c int = 1, 1, 0
 	for i := 0; i < 10; i++ {
 		fmt.Println(a)
